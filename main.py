7074e2ec23d8ac3cfe4cd696f8b928254a1ad03c
AnargyaDebug <73513088+AnargyaDebug@users.noreply.github.com>
2023-08-02 19:34:53 +0700

    Create main.py
    e

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+characters: str = "+-/*!&$#?=@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+pass_len: int = int(input("What will be the length of your password? "))
+generated_password: str = ""
+
+for i in range(pass_len):
+    generated_char: str = random.choice(characters)
+    generated_password += generated_char
+
+print(f"Generated password: {generated_password}")
